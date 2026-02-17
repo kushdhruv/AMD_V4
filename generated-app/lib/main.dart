48e97259770aefc83cff366d9ce371d5d5bf3bf4
Dhruv Kushwaha <dhruvstudy77@gmail.com>
2026-02-17 15:22:52 +0530

    Auto-generate Flutter App

--- generated-app/lib/main.dart
+++ generated-app/lib/main.dart
@@ -12,8 +12,8 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
   await Supabase.initialize(
-    url: 'undefined',
-    anonKey: 'undefined',
+    url: 'https://yyczfgxkprkxiafdencw.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl5Y3pmZ3hrcHJreGlhZmRlbmN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5ODkwMDQsImV4cCI6MjA4NjU2NTAwNH0.BlQWFPjnzsk6dK11ZvCFxT8ZD0eEgxq0-EMWBE-5Wh8',
   );
 
   runApp(const MyApp());
